feee12f927363183c7ddbd9846a3203cb7798b09
Olivier Barais <olivieR.barais@gmail.com>
2024-09-23 15:51:42 +0200

    update

---
+++ docs/assets/slidev/DrawingPreview-Bdfr1r2T.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-agxmcF41.js";import"../modules/vue-Texq-_OE.js";import"../index-DNUKq81O.js";import"../modules/shiki-DAyJrPKE.js";export{o as default};
---
+++ docs/assets/slidev/default-CGHXBD1B.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Texq-_OE.js";import{_ as r}from"../index-DNUKq81O.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
